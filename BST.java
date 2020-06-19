436c8369a39ea139f37552f57f54dec93fbbee69
yuewang0321 <yuewang0321@gmail.com>
2020-06-19 15:00:24 -0400

    longestunivaluepath

--- BST.java
+++ BST.java
@@ -156,4 +156,27 @@ class Solution {
         
         return Math.max(left,right)+root.val;
     }
+
+    /* Leetcode 687. Longest Univalue Path
+    Given a binary tree, find the length of the longest path where each node in the path has the same value. 
+    This path may or may not pass through the root.
+    The length of path between two nodes is represented by the number of edges between them.
+    */
+    int longest = 0;
+    public int longestUnivaluePath(TreeNode root) {
+        if (root==null) return 0;
+        helper(root, root.val);
+        return longest;    
+    }
+    public int helper(TreeNode root, int value) {
+        if (root==null) return 0;
+        
+        int left = helper(root.left, root.val);
+        int right = helper(root.right, root.val);
+        //longest = 左边+右边   如果某一边没有相同元素则为0
+        longest = Math.max(left+right, longest);
+        // 如果现在的val==prevval, then we need to pick a side which is longer
+        if (root.val == value) return Math.max(left, right)+1;
+        else return 0;
+    }
 }
