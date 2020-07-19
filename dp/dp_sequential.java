3b46509b889a5993a13fc812e2b6a4d8ef20d0ec
Yue Wang <yuewang@Yues-MacBook-Pro.local>
2020-07-19 09:38:48 -0400

    coin change

--- dp/dp_sequential.java
+++ dp/dp_sequential.java
@@ -30,4 +30,30 @@ class Solution {
         memory.put(s, false);
         return false;
     }
+
+    /* Leetcode 322. Coin Change
+    You are given coins of different denominations and a total amount of money amount. 
+    Write a function to compute the fewest number of coins that you need to make up that amount. 
+    If that amount of money cannot be made up by any combination of the coins, return -1.
+    Input: coins = [1, 2, 5], amount = 11
+    Output: 3 
+    Explanation: 11 = 5 + 5 + 1
+    https://www.youtube.com/watch?v=jgiZlGzXMBw&feature=emb_title
+
+    dp[i]代表达到目前amount需要的最小coin个数
+    dp[i] = Math.min(dp[i-c]+1, dp[i]), where c is nums in coin
+    */
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0] = 0;
+        for (int i=1; i<=amount; i++) {
+            for (int c: coins) {
+                if (c<=i) {
+                    dp[i] = Math.min(dp[i-c]+1, dp[i]);
+                }
+            }
+        }
+        return (dp[amount]==amount+1 ? -1 : dp[amount]);
+    }
 }
