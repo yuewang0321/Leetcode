830bb9c2dd6a10bd5a1898749c41ee6bd63e25e9
Yue Wang <yuewang@Yues-MacBook-Pro.local>
2020-07-19 14:44:57 -0400

    coin change 2

--- dp/dp_sequential.java
+++ dp/dp_sequential.java
@@ -105,4 +105,24 @@ class Solution {
         }
         return false;
     }
+
+    /* Leetcode 518. Coin Change 2
+    You are given coins of different denominations and a total amount of money. Write a function to compute the number 
+    of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+
+    Permuatation: use coin change's idea
+    Combination: iterate through coin first, then add previous to avoid same elements
+    */
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for (int c : coins) {
+            for (int i=1; i<dp.length; i++) {
+                if (c<=i) {
+                    dp[i] += dp[i-c];
+                }
+            }
+        }
+        return dp[dp.length-1];
+    }
 }
